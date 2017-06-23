fae156e999180390c75ed2cceebb5de386ff945f
GxiHub <gxi0306@hotmail.com>
2017-06-23 10:27:43 +0800

    Remove extra employee
    移除多餘的人事記錄

--- server.js
+++ server.js
@@ -15,21 +15,12 @@ SettingPage= require('./setting');
 var sleep = require('system-sleep');
 
 
-var 阿廣職稱 = '兼職'; var 阿廣上班店名 = '滴果'; var 阿廣上班位置 = '中山';
 var 小香職稱 = '兼職'; var 小香上班店名 = '滴果'; var 小香上班位置 = '中山';
-var 雲翔職稱 = '兼職'; var 雲翔上班店名 = '滴果'; var 雲翔上班位置 = '中山';
 var 竹竹職稱 = '兼職'; var 竹竹上班店名 = '滴果'; var 竹竹上班位置 = '中山';
-var 芳芳職稱 = '兼職'; var 芳芳上班店名 = '滴果'; var 芳芳上班位置 = '中山';
 var 藝芳職稱 = '兼職'; var 藝芳上班店名 = '滴果'; var 藝芳上班位置 = '中山';
 var 蓁蓁職稱 = '兼職'; var 蓁蓁上班店名 = '滴果'; var 蓁蓁上班位置 = '中山';
 var 芸賢職稱 = '兼職'; var 芸賢上班店名 = '滴果'; var 芸賢上班位置 = '中山';
 
-
-var 阿廣職稱 = '兼職'; var 阿廣上班店名 = '牧石'; var 阿廣上班位置 = '桃園';
-var 小乖職稱 = '兼職'; var 小乖上班店名 = '牧石'; var 小乖上班位置 = '桃園';
-var 士賢職稱 = '兼職'; var 士賢上班店名 = '牧石'; var 士賢上班位置 = '桃園';
-var 小新職稱 = '兼職'; var 小新上班店名 = '牧石'; var 小新上班位置 = '桃園';
-var 阿昌職稱 = '兼職'; var 阿昌上班店名 = '牧石'; var 阿昌上班位置 = '桃園';
 
 
 var db;
@@ -189,20 +180,6 @@ app.get('/stephyoffline/',function(req,res){
   res.redirect('/');
 });
 
-app.get('/nickonline/',function(req,res){
-  var namePass = '雲翔'; var statusPass = '上班';  brandTitle = 雲翔職稱 ; brandName = 雲翔上班店名; brandPlace = 雲翔上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
-app.get('/nickoffline/',function(req,res){
-  var namePass = '雲翔'; var statusPass = '下班';   brandTitle = 雲翔職稱 ; brandName = 雲翔上班店名; brandPlace = 雲翔上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
 app.get('/joyceonline/',function(req,res){
   var namePass = '竹竹'; var statusPass = '上班';   brandTitle = 竹竹職稱 ; brandName = 竹竹上班店名; brandPlace = 竹竹上班位置;
   SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
@@ -231,19 +208,6 @@ app.get('/yvonneoffline/',function(req,res){
   res.redirect('/');
 });
 
-app.get('/tiffanyonline/',function(req,res){
-  var namePass = '芳芳'; var statusPass = '上班';  brandTitle = 芳芳職稱 ; brandName = 芳芳上班店名; brandPlace = 芳芳上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
-app.get('/tiffanyoffline/',function(req,res){
-  var namePass = '芳芳'; var statusPass = '下班';  brandTitle = 芳芳職稱 ; brandName = 芳芳上班店名; brandPlace = 芳芳上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
 
 app.get('/janeonline/',function(req,res){
   var namePass = '蓁蓁'; var statusPass = '上班';  brandTitle = 蓁蓁職稱 ; brandName = 蓁蓁上班店名; brandPlace = 蓁蓁上班位置;
@@ -272,88 +236,3 @@ app.get('/yunoffline/',function(req,res){
   sleep(1.5);
   res.redirect('/');
 });
-
-app.get('/jeffonline/',function(req,res){
-  var namePass = '阿廣'; var statusPass = '上班'; brandTitle = 阿廣職稱 ; brandName = 阿廣上班店名; brandPlace = 阿廣上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
-app.get('/jeffoffline/',function(req,res){
-  var namePass = '阿廣'; var statusPass = '下班'; brandTitle = 阿廣職稱 ; brandName = 阿廣上班店名; brandPlace = 阿廣上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
-app.get('/miconline/',function(req,res){
-  var namePass = '小乖'; var statusPass = '上班'; brandTitle = 小乖職稱 ; brandName = 小乖上班店名; brandPlace = 小乖上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
-app.get('/micoffline/',function(req,res){
-  var namePass = '小乖'; var statusPass = '下班';  brandTitle = 小乖職稱 ; brandName = 小乖上班店名; brandPlace = 小乖上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
-app.get('/samonline/',function(req,res){
-  var namePass = '士賢'; var statusPass = '上班';  brandTitle = 士賢職稱 ; brandName = 士賢上班店名; brandPlace = 士賢上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
-app.get('/samoffline/',function(req,res){
-  var namePass = '士賢'; var statusPass = '下班';   brandTitle = 士賢職稱 ; brandName = 士賢上班店名; brandPlace = 士賢上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
-app.get('/oswinonline/',function(req,res){
-  var namePass = '小新'; var statusPass = '上班';   brandTitle = 小新職稱 ; brandName = 小新上班店名; brandPlace = 小新上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
-app.get('/oswinoffline/',function(req,res){
-  var namePass = '小新'; var statusPass = '下班';   brandTitle = 小新職稱 ; brandName = 小新上班店名; brandPlace = 小新上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
-app.get('/audyonline/',function(req,res){
-  var namePass = '阿昌'; var statusPass = '上班';  brandTitle = 阿昌職稱 ; brandName = 阿昌上班店名; brandPlace = 阿昌上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
-app.get('/audyoffline/',function(req,res){
-  var namePass = '阿昌'; var statusPass = '下班';  brandTitle = 阿昌職稱 ; brandName = 阿昌上班店名; brandPlace = 阿昌上班位置;
-  SettingPage.EmployeeWorkTimeAndStatus(namePass,statusPass,brandTitle,brandName,brandPlace);
-  sleep(1.5);
-  res.redirect('/');
-});
-
-
-
-  // console.log('req.body.checkPeriodYear =',req.body.checkPeriodYear);
-  // console.log('req.body.checkPeriodMonth =',req.body.checkPeriodMonth);
-  // console.log('req.body.checkName =',req.body.checkName);
-
-  // console.log('req.body.UniqueID =',req.body.UniqueID);
-  // console.log('req.body.updathour =',req.body.updathour);
-  // console.log('req.body.updateminute =',req.body.updateminute);
-  // console.log('req.body.updateday =',req.body.updateday);
-  // console.log('req.body.updatemonth =',req.body.updatemonth);
-
-  // console.log('req.body.UniqueID =',req.body.UniqueID);
-  // console.log('req.body.DeleteType =',req.body.DeleteType);
